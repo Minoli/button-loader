466022f33080a16ffff7fa5325b07c613fc14d47
Minoli <minoli@wso2.com>
2015-05-12 10:55:48 +0530

    Update jquery.buttonLoader.js

--- jquery.buttonLoader.js
+++ jquery.buttonLoader.js
@@ -1,20 +1,27 @@
-/*A jQuery plugin which add loading indicators into buttons*/
-
+/*A jQuery plugin which add loading indicators into buttons
+* By Minoli Perera
+* MIT Licensed.
+*/
 (function ($) {
     $.fn.buttonLoader = function (action) {
         var self = $(this);
+        //start loading animation
         if (action == 'start') {
             if ($(self).attr("disabled") == "disabled") {
                 e.preventDefault();
             }
+            //disable buttons when loading state
             $('.has-spinner').attr("disabled", "disabled");
             $(self).attr('data-btn-text', $(self).text());
+            //binding spinner element to button and change button text
             $(self).html('<span class="spinner"><i class="fa fa-spinner fa-spin"></i></span>Loading');
             $(self).addClass('active');
         }
+        //stop loading animation
         if (action == 'stop') {
             $(self).html($(self).attr('data-btn-text'));
             $(self).removeClass('active');
+            //enable buttons after finish loading
             $('.has-spinner').removeAttr("disabled");
         }
     }
