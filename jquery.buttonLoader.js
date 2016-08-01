3b202ff5c21f642c271269b59aaa5b28ef7a6dab
minoli <minoliperera91@gmail.com>
2016-08-01 09:49:40 +0530

    Updates

--- jquery.buttonLoader.js
+++ jquery.buttonLoader.js
@@ -3,26 +3,27 @@
 * MIT Licensed.
 */
 (function ($) {
+    $('.has-spinner').attr("disabled", false);
     $.fn.buttonLoader = function (action) {
         var self = $(this);
-        //start loading animation
         if (action == 'start') {
             if ($(self).attr("disabled") == "disabled") {
-                e.preventDefault();
+                return false;
             }
-            //disable buttons when loading state
-            $('.has-spinner').attr("disabled", "disabled");
+            $('.has-spinner').attr("disabled", true);
             $(self).attr('data-btn-text', $(self).text());
-            //binding spinner element to button and changing button text
-            $(self).html('<span class="spinner"><i class="fa fa-spinner fa-spin"></i></span>Loading');
+            var text = 'Loading';
+            console.log($(self).attr('data-load-text'));
+            if($(self).attr('data-load-text') != undefined && $(self).attr('data-load-text') != ""){
+                var text = $(self).attr('data-load-text');
+            }
+            $(self).html('<span class="spinner"><i class="fa fa-spinner fa-spin" title="button-loader"></i></span> '+text);
             $(self).addClass('active');
         }
-        //stop loading animation
         if (action == 'stop') {
             $(self).html($(self).attr('data-btn-text'));
             $(self).removeClass('active');
-            //enable buttons after finish loading
-            $('.has-spinner').removeAttr("disabled");
+            $('.has-spinner').attr("disabled", false);
         }
     }
 })(jQuery);
